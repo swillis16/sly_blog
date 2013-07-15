a65a7474289c59432457793b30a6c8aaadf0f270
my-ubuntu <sylvester@ubuntu.(none)>
2013-07-15 06:04:46 -0700

    Restarting development of site.

---
+++ README.md
@@ -0,0 +1,5 @@
+Sly-Blog
+========
+
+This repo holds code for a blog/portfolio site that I am writing using Rails using the 
+[Getting Started With Rails](http://guides.rubyonrails.org/getting_started.html) tutorial.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SlyBlog::Application.config.secret_key_base = '9f59ee490d613800f268ffcd5929467517f130718e8d65d32293996319939e4981831fe06d6d994985aff76929b75bdbbe8f068afa13f1af5583fe77576b2953'
