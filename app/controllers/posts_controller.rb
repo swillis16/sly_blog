1c7d4ad8a69d9903f076df4e5bb904d3953aa9dc
my-ubuntu <sylvester@ubuntu.(none)>
2013-07-15 10:24:20 -0700

    Created handling for adding and showing of posts.

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,27 @@
+class PostsController < ApplicationController
+
+def index
+    @posts = Post.all()
+end
+
+def new
+
+end
+
+def create 
+    @post = Post.new(post_params)
+    postSaved = @post.save
+
+    redirect_to @post
+end
+
+def show
+    @post = Post.find(params[:id])
+end
+
+private
+    def post_params
+        params.require(:post).permit(:title, :text)
+    end
+
+end
