efd91ae479fcd348c6b7db6ce59bc7063d71c317
Sylvester Willis <swillis16@student.gsu.edu>
2013-07-19 12:27:33 -0400

    Added gravatar to header and made some minor tweaks to the CSS.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
+require 'digest'
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  layout 'application'
+  before_filter :set_constants
+
+  def set_constants
+    @emailHash = Digest::MD5.hexdigest('swillis16@student.gsu.edu')
+  end
 end
