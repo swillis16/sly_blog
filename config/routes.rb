ba596c0f808ae074c3731c4e30f96e0b5cb7b136
my-ubuntu <sylvester@ubuntu.(none)>
2013-07-22 06:29:33 -0700

    Generated model for commenting on posts.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ SlyBlog::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'posts#index'
 
   resources :posts
 
